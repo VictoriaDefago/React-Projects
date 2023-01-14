c79b8deb1cef6ecaec518868549e4c79a3a160b6
VictoriaDefagot <defagotvictoria@gmail.com>
2023-01-14 19:36:49 -0300

    Circular progress bar

--- expense-planner-vite/src/components/ExpenseControl.jsx
+++ expense-planner-vite/src/components/ExpenseControl.jsx
@@ -1,16 +1,25 @@
 import { useEffect, useState } from 'react'
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar'
+import 'react-circular-progressbar/dist/styles.css'
 
 const ExpenseControl = ({ budget, expenses }) => {
 
     const [available, setAvailable] = useState(0)
     const [spent, setSpent] = useState(0)
+    const [percentage, setPercentage] = useState(0)
 
     useEffect( () => {
         const totalSpent = expenses.reduce( (total, expense) => expense.qty + total, 0)
         const totalAvailable = budget - totalSpent
 
+        const newPercentage = ((budget - totalAvailable) / budget * 100).toFixed(2)
+
         setSpent(totalSpent)
         setAvailable(totalAvailable)
+
+        setTimeout( () => {
+            setPercentage(newPercentage)
+          }, 1500)
 
     }, [expenses])
 
@@ -21,7 +30,11 @@ const ExpenseControl = ({ budget, expenses }) => {
   return (
     <div className="contenedor-presupuesto contenedor sombra dos-columnas">
         <div>
-            Grafica
+            <CircularProgressbar value={percentage} styles={buildStyles({
+                pathColor: '#3B82F6',
+                trailColor: '#F5F5F5',
+                textColor: '#3B82F6',
+            })} text={`${percentage}% Gastado`} />
         </div>
         <div className="contenido-presupuesto">
             <p>
